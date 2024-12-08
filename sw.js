3dbeed4172c1ac6a40e8feea88b5827b01be0538
giltterglose <2455219437@qq.com>
2024-12-08 20:07:03 +0800

    Site updated: 2024-12-08 20:07:01

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733579875188";
+const VERSION = "1733659615119";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
